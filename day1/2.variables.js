f12fdfa01b465282273336a35a77cae61cbab96f
uzairahmed2126 <uzairahmed2126@gmail.com>
2025-08-23 16:05:22 +0530

    variable.

---
+++ day1/2.variables.js
@@ -0,0 +1,18 @@
+age  = 0;
+console.log(age);// it is treat as a var variable
+// var age = 21;
+let fName = "Uzair";
+const lName = "Ahmed";
+
+//var can be re-declare
+// var age  = 20;
+
+//var and let can be re-assign
+// age  = 22;
+fName = "zubair"
+
+
+console.log(age,fName,lName)
+// const is can't be re-declare and re-assign
+
+// lName = "hello" //type error will occur 

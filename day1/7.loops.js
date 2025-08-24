dc0c8a296a68aeff25283a17a6a6dea0ca24f1b6
uzairahmed2126 <uzairahmed2126@gmail.com>
2025-08-24 16:29:18 +0530

    loops

---
+++ day1/7.loops.js
@@ -0,0 +1,16 @@
+// loops to iterate data
+// do while loop execute atleast one time either condition is true or false
+let i = 0;
+do {
+    i += 1;
+} while (i < 5) {
+    console.log(i)
+}
+for (let i = 0; i < 10; i++) {
+    console.log(i);
+}
+i = 0;
+while (i < 5) {
+    console.log(i);
+    i++;
+}
